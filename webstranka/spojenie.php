e0adec2545b65506e4073010ee9008cf45c414f4
zorvar <zorvar@gmail.com>
2015-10-14 22:56:16 +0200

    Create spojenie.php

---
+++ webstranka/spojenie.php
@@ -0,0 +1,14 @@
+ <?php echo "Dnes je <b>".date("j.n.20y.")."</b><br>\n"; 
+
+   $host        = "host=127.0.0.1";
+   $port        = "port=5432";
+   $dbname      = "dbname=konpoz";
+   $credentials = "user=aiex password=veslo";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db){
+      echo "Error : spojenie s databazou je nateraz nefunkcne \n";
+   } else {
+      echo "&nbsp &nbsp <br>Spojenie s databazou je funkcne <br><br>";
+   }
+?>
